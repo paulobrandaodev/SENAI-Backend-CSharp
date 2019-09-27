8011947efb9d3f6e8539711e942ac3872bde53f4
Paulo Brandão <paulobrandaoofficial@gmail.com>
2019-09-27 10:33:57 -0300

    Aula8 Adicionada

---
+++ Aula8-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero;
+            
+            try{
+
+                // Escrevemos um bloco de código
+                Console.WriteLine("Digite um número");
+                numero = int.Parse(Console.ReadLine());
+
+            }catch(Exception ex){
+                
+                // Mostramos a exceção da regra
+                // Erro gerado na aplicação
+                Console.WriteLine("Ops :( Erro: " + ex);
+
+            }
+
+            
+
+
+        }
+    }
+}
---
+++ Aula8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] v1 = { "A", "B", "C", "D", "E" };
+          
+            // Laço FOREACH (para cada)
+            foreach(string xpto in v1){
+                Console.WriteLine(xpto);
+            }
+
+            
+        }
+    }
+}
